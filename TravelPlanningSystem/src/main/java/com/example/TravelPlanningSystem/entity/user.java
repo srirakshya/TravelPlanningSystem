23d2297ddf3582c6739fa82985bad391677e85b8
srira <srira@Rakshu>
2024-02-10 18:53:03 +0530

    Project created

---
+++ TravelPlanningSystem/src/main/java/com/example/TravelPlanningSystem/entity/user.java
@@ -0,0 +1,44 @@
+package com.example.TravelPlanningSystem.entity;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Component
+public class user {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private String userPassword;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+	
+
+
+}
