5aaa879bce8c52d8ae6ce388dc365b6099d5075c
srira <srira@Rakshu>
2024-02-13 09:46:19 +0530

    Dao created..!

--- TravelPlanningSystem/src/main/java/com/example/TravelPlanningSystem/Dao/UserDao.java
+++ TravelPlanningSystem/src/main/java/com/example/TravelPlanningSystem/Dao/UserDao.java
@@ -1,5 +1,56 @@
 package com.example.TravelPlanningSystem.Dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.TravelPlanningSystem.entity.User;
+import com.example.TravelPlanningSystem.repo.UserRepository;
+
 public class UserDao {
+	
+	@Autowired
+	UserRepository repo;
+
+	public User saveUser(User user)
+	{
+		return repo.save(user);
+	}
+	
+	public User findUser(int userId)
+	{
+		Optional<User> opUser = repo.findById(userId);
+		
+		if(opUser.isPresent())
+		{
+			return opUser.get();
+		}
+		return null;
+	}
+	
+	public User deleteUser(int userId)
+	{
+		User user = findUser(userId);
+		repo.delete(user);
+		return user;
+	}
+	
+	public User updateUser(User user,int userId)
+	{
+		User exUser = findUser(userId);
+		if(exUser != null)
+		{
+			user.setUserId(userId);
+			return repo.save(user);
+		}
+		return null;
+	}
+	
+	public List<User> findAllUsers()
+	{
+		return repo.findAll();
+	}
+	
 
 }
---
+++ TravelPlanningSystem/src/main/java/com/example/TravelPlanningSystem/entity/User.java
@@ -0,0 +1,81 @@
+package com.example.TravelPlanningSystem.entity;
+
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
+@Entity
+@Component
+
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Trip>trips;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public List<Trip> getTrips() {
+		return trips;
+	}
+
+	public void setTrips(List<Trip> trips) {
+		this.trips = trips;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPassword="
+				+ userPassword + "]";
+	}
+	
+	
+
+}
